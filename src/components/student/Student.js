a1de8447c8782941ea5914826e1c3e76d96a201d
khoavo13 <vandangkhoa.vo@gmail.com>
2024-09-11 17:30:27 +0700

    first commit

---
+++ src/components/student/Student.js
@@ -0,0 +1,231 @@
+import { Button, Container, Table, Alert, Input } from "reactstrap";
+import { useDispatch, useSelector } from 'react-redux';
+import { getAll, deleteStudent, resetStatusAndMessage, editStudent } from "../../redux/studentSlice";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ReactPaginate from 'react-paginate';
+
+export default function Student() {
+    const [currentPage, setCurrentPage] = useState(0);
+    const [showMessage, setShowMessage] = useState(false); // Local state to control message visibility
+    const handlePageClick = (event) => {
+        setCurrentPage(event.selected);
+    };
+    const dispatch = useDispatch();
+    const { totalPages, students, status, message, error } = useSelector((state) => state.student);
+
+    const limit = 5;
+    useEffect(() => {
+        dispatch(getAll({ currentPage, limit }));
+    }, [currentPage, dispatch]);
+
+    useEffect(() => {
+        if (status && message) {
+            setShowMessage(true);
+
+            const timer = setTimeout(() => {
+                setShowMessage(false);
+                dispatch(resetStatusAndMessage()); // Reset status and message
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [status, message, dispatch]);
+
+    const handle_delete = (id) => {
+        dispatch(deleteStudent(id));
+    };
+
+    const XepLoaiEnum = {
+        GIOI: "Giỏi",
+        KHA: "Khá",
+        TRUNG_BINH: "Trung bình",
+        YEU: "Yếu"
+    };
+
+
+    const convertToValue = (enumCode) => {
+        switch (enumCode) {
+            case "Gioi":
+                return XepLoaiEnum.GIOI;
+            case "KHA":
+                return XepLoaiEnum.KHA;
+            case "TRUNG_BINH":
+                return XepLoaiEnum.TRUNG_BINH;
+            case "YEU":
+                return XepLoaiEnum.YEU;
+            default:
+                return null;
+        }
+    };
+
+    const [EStudent, setEStudent] = useState({ id: "", ten: "", thanhPho: "", ngaySinh: "", xepLoai: "" })
+    const [studentEdit, setStudentEdit] = useState({ isEdit: false, id: "" })
+    const handle_edit = (id, item) => {
+        console.log(item)
+        setStudentEdit({ isEdit: true, id })
+        setEStudent(item)
+       
+    }
+    const handle_save = (id) => {
+        dispatch(editStudent({
+            id,
+            student: {
+                ...EStudent,
+                ngaySinh: EStudent.ngaySinh, // Sử dụng định dạng YYYY-MM-DD
+                xepLoai: EStudent.xepLoai // Sử dụng giá trị phù hợp với enum
+            }
+
+        })
+
+        );
+        setStudentEdit({ isEdit: false, id :""})
+
+    }
+    const convertDateToYYYYMMDD = (date) => {
+        const [day, month, year] = date.split('-');
+        return `${year}-${month}-${day}`;
+    };
+
+    const convertDateToDDMMYYYY = (date) => {
+        const [year, month, day] = date.split('-');
+        return `${day}-${month}-${year}`;
+    };
+    console.log(EStudent)
+    return (
+        <div className="products">
+           
+            <Container>
+                {showMessage && (
+                    <Alert color={status === 200 ? "success" : "danger"}>
+                        {message}
+                    </Alert>
+                )}
+
+                <Table hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th> ID</th>
+                            <th>Tên</th>
+                            <th>Thành phố</th>
+                            <th>Ngày sinh</th>
+                            <th>Xếp loại</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {students && students.map((item, index) => (
+                            <tr key={index} className={studentEdit.isEdit && item.id === studentEdit.id ? "student-item active" : "student-item"}>
+                                <th scope="row">{index + 1}</th>
+                                <td>
+                                    {studentEdit.isEdit && item.id === studentEdit.id ?
+                                        <Input type="hidden" value={EStudent.id} 
+                                            onChange={(e) => setEStudent({ ...EStudent, id: e.target.value })}
+                                        />
+                                        :
+                                        item.id
+                                    }
+                                </td>
+                                <td>
+                                    {studentEdit.isEdit && item.id === studentEdit.id ?
+                                        <Input type="text" value={EStudent.ten}
+                                            onChange={(e) => setEStudent({ ...EStudent, ten: e.target.value })}
+                                        />
+                                        :
+                                        item.ten
+                                    }
+                                </td>
+                                <td>
+                                    {studentEdit.isEdit && item.id === studentEdit.id ?
+                                        <Input type="text" value={EStudent.thanhPho}
+                                            onChange={(e) => setEStudent({ ...EStudent, thanhPho: e.target.value })}
+                                        />
+                                        :
+                                        item.thanhPho
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        studentEdit.isEdit && item.id === studentEdit.id ?
+                                            <Input
+
+                                                type="date"
+                                                value={EStudent.ngaySinh} 
+                                                onChange={(e) => setEStudent({ ...EStudent, ngaySinh: e.target.value })}
+                                            />
+                                            :
+                                            item.ngaySinh
+
+                                    }
+                                </td>
+
+                                <td>
+                                    {studentEdit.isEdit && item.id === studentEdit.id ?
+                                        <Input
+                                            id="xepLoai"
+                                            name="xepLoai"
+                                            type="select"
+                                            value={EStudent.xepLoai}
+                                            onChange={(e) => setEStudent({ ...EStudent, xepLoai: e.target.value })}
+                                        >
+                                            <option value="GIOI">Giỏi</option>
+                                            <option value="KHA">Khá</option>
+                                            <option value="TRUNG_BINH">Trung bình</option>
+                                            <option value="YEU">Yếu</option>
+                                        </Input>
+                                        :
+                                        convertToValue(item.xepLoai)
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        studentEdit.isEdit && item.id === studentEdit.id ?
+                                            <Button className="btn btn-success"
+                                                onClick={() => handle_save(item.id)}
+                                            >Save </Button>
+                                            :
+                                            <>
+                                                <Button
+                                                    className="btn btn-danger"
+                                                    onClick={() => {
+                                                        if (window.confirm('Are you sure you want to delete this student?')) {
+                                                            handle_delete(item.id);
+                                                        }
+                                                    }}
+                                                >
+                                                    <i className="fa-solid fa-delete-left"></i>
+                                                </Button>
+                                                <Button className="btn btn-success" onClick={() => handle_edit(item.id, item)}>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                </Button>
+                                            </>
+                                    }
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                <ReactPaginate
+                    previousLabel={'Previous'}
+                    nextLabel={'Next'}
+                    breakLabel={'...'}
+                    pageCount={Math.ceil(totalPages)}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={'pagination'}
+                    pageClassName={'page-item'}
+                    pageLinkClassName={'page-link'}
+                    previousClassName={'page-item'}
+                    nextClassName={'page-item'}
+                    previousLinkClassName={'page-link'}
+                    nextLinkClassName={'page-link'}
+                    breakClassName={'page-item'}
+                    breakLinkClassName={'page-link'}
+                    activeClassName={'active'}
+                />
+            </Container>
+        </div>
+    );
+}
---
+++ src/redux/studentSlice.js
@@ -0,0 +1,106 @@
+// src/features/auth/authSlice.js
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+// Thay đổi URL và cấu hình phù hợp với API của bạn
+const BASE_URL = 'http://localhost:8080';
+
+export const getAll= createAsyncThunk('student/getAll', async ({ currentPage, limit },thunkAPI) => {
+  const url= BASE_URL+`/student/list?page=${currentPage}&size=${limit}`;
+  try {
+    const response = await axios.get(url);
+    return response.data; // Trả về dữ liệu từ phản hồi
+
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data); // Trả về lỗi nếu có
+  }
+});
+export const addNewStudent= createAsyncThunk('student/addNewStudent', async (student,thunkAPI) => {
+  const url= BASE_URL+`/student`;
+  try {
+    const response = await axios.post(url, student);
+    return response.data; // Trả về dữ liệu từ phản hồi
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data); // Trả về lỗi nếu có
+  }
+});
+export const deleteStudent= createAsyncThunk('student/deleteStudent', async (id,thunkAPI) => {
+   const url= BASE_URL+`/student/${id}`;
+  try {
+    const response = await axios.delete(url);
+    return response.data; // Trả về dữ liệu từ phản hồi
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data); // Trả về lỗi nếu có
+  }
+});
+
+export const editStudent= createAsyncThunk('student/editProduct', async ({id,student},thunkAPI) => {
+  const url= BASE_URL+`/student/${id}`;
+  try {
+    console.log(student)
+    const response = await axios.put(url,student);
+    return response.data; // Trả về dữ liệu từ phản hồi
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data); // Trả về lỗi nếu có
+  }
+});
+const studentSlice = createSlice({
+  name: 'student',
+  initialState: {
+    status: 'idle',
+    error: null,
+    students:null,
+    totalPages:10,
+    message:"",
+  },
+  reducers: {
+    resetStatusAndMessage: (state) => {
+      state.status = null;
+      state.message = "";
+      state.error=null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAll.fulfilled, (state, action) => {
+        state.students = action.payload.data.students
+        state.totalPages = action.payload.data.totalPages
+        
+      })
+      .addCase(addNewStudent.fulfilled, (state, action) => {
+        state.status=action.payload.status
+        state.message=action.payload.message
+        state.students = [...state.students, action.payload.data];
+      })
+      .addCase(addNewStudent.rejected, (state, action) => {
+        state.status=action.payload.status
+        state.message=action.payload.message
+        state.error=action.payload.data
+      })
+      .addCase(deleteStudent.fulfilled, (state, action) => {
+        state.status=action.payload.status
+        state.message=action.payload.message
+        state.students = state.students.filter(student => student.id !== action.payload.data);
+      })
+      .addCase(deleteStudent.rejected, (state, action) => {
+        state.status=action.payload.status
+        state.message=action.payload.message
+        state.error=action.payload.data
+      })
+      .addCase(editStudent.fulfilled, (state, action) => {
+        state.status=action.payload.status
+        state.message=action.payload.message
+        state.students = state.students.map(student =>
+          student.id === action.payload.data.id ? action.payload.data : student
+      );
+      })
+      .addCase(editStudent.rejected, (state, action) => {
+        state.status=action.payload.status
+        state.message=action.payload.message
+        state.error=action.payload.data
+      })
+      
+     ;
+  },
+});
+export const { resetStatusAndMessage } = studentSlice.actions;
+export default studentSlice.reducer;
